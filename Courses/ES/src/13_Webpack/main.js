34e5a68a14ea2cfdf4da000c3689c252ecf16978
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-04-03 21:08:41 -0300

    Started Webpack classes

---
+++ Courses/ES/src/13_Webpack/main.js
@@ -0,0 +1,6 @@
+import * as calculator from './calculator'
+import mult from './multiply'
+
+console.log( calculator.sum(5, 5) )
+console.log( calculator.substraction(10, 3) )
+console.log( mult(6, 3) );
---
+++ Courses/ES/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: ['@babel/polyfill', path.resolve(__dirname, './src/13_Webpack/main.js')],
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: './public',
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Development' }),
+  ],
+}
