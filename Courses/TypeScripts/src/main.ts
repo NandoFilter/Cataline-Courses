309c40043d434ef148dc4e02c2bee0560d3b1d2c
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-06-12 18:46:28 -0300

    Second class about Types

--- Courses/TypeScripts/src/main.ts
+++ Courses/TypeScripts/src/main.ts
@@ -4,4 +4,19 @@ let message: string = 'Hello World'
 let total: number = 0
 let isOpen: boolean = true
 
-console.log(`${message} | ${total} | ${isOpen}`);
+console.log(`${message} | ${total} | ${isOpen}`)
+
+// Any, Void & Never
+
+let item: any = 89
+item = null
+item = 'Hello World'
+item = false
+
+function showInformation(name: string = 'Fernando'): void {
+    console.log(name)
+}
+
+function error(): never {
+    throw new Error('Error')
+}
