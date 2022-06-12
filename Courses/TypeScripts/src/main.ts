8b5f914c8ed4cddb73b71bfc9403fc428743206e
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-06-12 18:30:21 -0300

    First class about types

--- Courses/TypeScripts/src/main.ts
+++ Courses/TypeScripts/src/main.ts
@@ -1,1 +1,7 @@
-console.log('Hello World');
+// String, Number & Boolean
+
+let message: string = 'Hello World'
+let total: number = 0
+let isOpen: boolean = true
+
+console.log(`${message} | ${total} | ${isOpen}`);
