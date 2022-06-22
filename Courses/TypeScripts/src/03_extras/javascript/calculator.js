e8ca6913b9b21e8d93d37419b8528f6575db5f6a
Fernando H. Filter <ff@interact.com.br>
2022-06-22 14:30:34 -0300

    Allowing JavaScript files on project

---
+++ Courses/TypeScripts/src/03_extras/javascript/calculator.js
@@ -0,0 +1,7 @@
+export const sum = (a, b) => {
+    return a + b;
+}
+
+export const substraction = (a, b) => {
+    return a - b;
+}
---
+++ Courses/TypeScripts/src/03_extras/javascript/main.ts
@@ -0,0 +1,4 @@
+// Habilitar allowJs em tsconfig.json
+import { sum } from './calculator';
+
+console.log(sum(5, 5));
