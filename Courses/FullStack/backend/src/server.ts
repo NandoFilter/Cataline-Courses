467723513b6fd627c7d9ea09d7f58c1196ee12e7
Fernando H. Filter <ff@interact.com.br>
2022-06-29 16:37:45 -0300

    Working with Param Types

--- Courses/FullStack/backend/src/server.ts
+++ Courses/FullStack/backend/src/server.ts
@@ -4,17 +4,25 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 // Métodos HTTP -> GET | POST | PUT | DELETE
 
+// Tipos de parâmetros
+// -------------------
+// Query Params | /users?perPage=3&page=1
+// Route Params | /users/:id -> /users/1
+// Request Body | { "name": "Fernando" }
+
 app.get('/users', (req: Request, res: Response) => {
-    return res.json(['user_1', 'user_2']);
+    return res.json(["user_1", "user_2"]);
 });
 
 app.post('/users', (req: Request, res: Response) => {
     return res.json({ message: 'Criando usuário' });
 });
 
-app.put('/users', (req: Request, res: Response) => {
+app.put('/users/:id', (req: Request, res: Response) => {
     return res.json({ message: 'Atualizando usuário' });
 });
 
