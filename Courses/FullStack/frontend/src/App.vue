d8380e69e6fa3e01ff7f96f0e6d10b7ba4011daf
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-07-03 21:35:15 -0300

    Creating a component

--- Courses/FullStack/frontend/src/App.vue
+++ Courses/FullStack/frontend/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>Hello {{ name }}</h1>
+  <Header></Header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from '@/components/Header.vue'
 
 export default defineComponent({
+  components: {
+    Header,
+  },
   data() {
     return {
       name: 'Fernando',
