113f70e1b14d1192e983c069cea2109a84bfbc86
Fernando H. Filter <ff@interact.com.br>
2022-07-07 16:35:08 -0300

    Starting Vue CLI project

---
+++ Courses/Vue/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello {{ person.name }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface Person {
+  name: string
+  age: number
+}
+
+export default defineComponent({
+  data() {
+    return {
+      person: {} as Person,
+    }
+  },
+})
+</script>
