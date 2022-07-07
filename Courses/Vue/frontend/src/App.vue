f1751f9ded8aa933a4f2d535ca3824232ff2f6c7
Fernando H. Filter <ff@interact.com.br>
2022-07-07 17:59:03 -0300

    Using beforeCreated and created hooks

--- Courses/Vue/frontend/src/App.vue
+++ Courses/Vue/frontend/src/App.vue
@@ -1,52 +1,22 @@
 <template>
-  <p v-for="todo in doneTodos" :key="todo.text">
-    {{ todo.text }}
-  </p>
-
-  <button @click="checkAllTodos">Finalizar</button>
+  <h1>{{ message }}</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-interface Todo {
-  text: string
-  done: boolean
-}
-
 export default defineComponent({
   data() {
     return {
-      todos: [] as Todo[],
+      message: 'Hello World',
+      posts: [],
     }
   },
-  computed: {
-    doneTodos(): Todo[] {
-      return this.todos.filter((todo) => todo.done)
-    },
-  },
-  watch: {
-    todos(newValue: Todo[]) {
-      const isFinished = !newValue.some(({ done }) => !done)
-
-      if (isFinished) {
-        alert('Todas as tarefas foram finalizadas!')
-      }
-    },
+  beforeCreate() {
+    console.log(this.message) // undefined
   },
   created() {
-    this.todos = [
-      { text: 'Estudar Typescript', done: true },
-      { text: 'Lavar os pratos', done: false },
-      { text: 'Aprender Nuxt.js', done: true },
-    ]
-  },
-  methods: {
-    checkAllTodos() {
-      this.todos = this.todos.map(({ text }) => {
-        return { text, done: true }
-      })
-    },
+    console.log(this.message) // 'Hello World'
   },
 })
 </script>
