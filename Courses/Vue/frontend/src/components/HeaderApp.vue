a83ed21edb2a77be09cdc0ecabda1afc16dca13f
Fernando H. Filter <ff@interact.com.br>
2022-07-13 15:22:39 -0300

    Creating Header and another components

---
+++ Courses/Vue/frontend/src/components/HeaderApp.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <nav>
+      <button>Life Cycle</button>
+      <button>Properties</button>
+    </nav>
+  </header>
+</template>
---
+++ Courses/Vue/frontend/src/components/LifeCycles.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>{{ message }}</h1>
+  <h1>{{ count }}</h1>
+
+  <button @click="count++">Clique</button>
+  <button @click="destroyComponent">Desmontar</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      message: 'Hello World',
+      count: 0,
+    }
+  },
+  beforeCreate() {
+    // Sem acesso aos dados
+    console.log(this.message) // undefined
+  },
+  created() {
+    // Com acesso aos dados
+    console.log(this.message) // 'Hello World'
+  },
+  beforeMount() {
+    // Sem acesso a DOM
+    console.log(this.$el) // null
+  },
+  mounted() {
+    // Com acesso a DOM
+    console.log(this.$el) // <h1>Hello World</h1>
+  },
+  beforeUpdate() {
+    // Ocorre antes da mudança de algum dado
+    console.log('Antes da atualização')
+  },
+  updated() {
+    // Ocorre após a mudança de algum dado
+    console.log('Depois da atualização')
+  },
+  beforeUnmount() {
+    // Ocorre antes de destruir um componente
+    console.log('Antes de desmontar')
+  },
+  unmounted() {
+    // Ocorre após destruir um componente
+    console.log('Depois de desmontar')
+  },
+  methods: {
+    destroyComponent() {
+      this.$.appContext.app.unmount()
+    },
+  },
+})
+</script>
