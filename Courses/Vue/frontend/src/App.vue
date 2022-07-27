d7277349d38cc8e41c5d25bf57b5a4ded1a7606e
Fernando H. Filter <ff@interact.com.br>
2022-07-27 14:54:25 -0300

    Creating a dynamic component

--- Courses/Vue/frontend/src/App.vue
+++ Courses/Vue/frontend/src/App.vue
@@ -8,18 +8,33 @@
       <i class="fa-brands fa-facebook-f"></i>
     </template>
   </ButtonStyled>
+
+  <ul>
+    <li @click="currentView = 'Home'">Home</li>
+    <li @click="currentView = 'About'">Sobre</li>
+    <li @click="currentView = 'Contact'">Contato</li>
+  </ul>
+
+  <component :is="currentView" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
+
 export default defineComponent({
+  components: { Home, About, Contact },
   data() {
     return {
       user: {
         id: 1,
         name: 'Fernando',
       },
+
+      currentView: 'Home',
     }
   },
   methods: {
