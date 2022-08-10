af84a41540bf07312896240f4aaa74ba267db365
Fernando H. Filter <ff@interact.com.br>
2022-08-10 14:47:37 -0300

    Created Services organism

--- Courses/atomic-design/src/App.vue
+++ Courses/atomic-design/src/App.vue
@@ -2,17 +2,19 @@
   <div class="component">
     <Header />
     <Presentation />
+    <Services />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Header, Presentation } from '@/components/organisms'
+import { Header, Presentation, Services } from '@/components/organisms'
 
 export default defineComponent({
   components: {
     Header,
-    Presentation
+    Presentation,
+    Services
   }
 })
 </script>
---
+++ Courses/atomic-design/src/components/organisms/Services.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur magni impedit est in! Incidunt ullam minima corporis quas quidem id quis consectetur nemo, assumenda tempore molestias culpa aliquid necessitatibus quam."
+    />
+    <Service
+      title="Back End"
+      description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur magni impedit est in! Incidunt ullam minima corporis quas quidem id quis consectetur nemo, assumenda tempore molestias culpa aliquid necessitatibus quam."
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur magni impedit est in! Incidunt ullam minima corporis quas quidem id quis consectetur nemo, assumenda tempore molestias culpa aliquid necessitatibus quam."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({
+  components: { Service }
+})
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
