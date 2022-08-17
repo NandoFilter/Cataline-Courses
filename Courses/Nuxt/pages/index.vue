508653caec9a7ff94ca647f061797ca661f82365
Fernando H. Filter <ff@interact.com.br>
2022-08-17 15:48:44 -0300

    Working with Head Tags

--- Courses/Nuxt/pages/index.vue
+++ Courses/Nuxt/pages/index.vue
@@ -9,6 +9,26 @@ import Vue from 'vue'
 
 export default Vue.extend({
   layout: 'website',
+  data() {
+    return {
+      title: 'Dynamic Title'
+    }
+  },
+  head(this: any) {
+    return {
+      title: this.title,
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
   mounted() {
     const posts = this.$axios.$get('/posts')
 
