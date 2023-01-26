3e7d1561c2f4e33849534cfe8a844408e2bc8585
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2023-01-26 00:13:36 -0300

    Created SocialLink Atoms and SocialNetwork and Service Molecules

--- Courses/atomic-design/src/App.vue
+++ Courses/atomic-design/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="app">
-    <SearchForm />
+    <Service
+      title="Front-End"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea repudiandae, labore eveniet maiores ipsa fugit, ad omnis hic laboriosam magni necessitatibus autem soluta recusandae aliquid similique doloribus! Suscipit, debitis odio!"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SearchForm } from '@/components/molecules'
+import { Service } from '@/components/molecules'
 
 export default defineComponent({
   components: {
-    SearchForm
+    Service
   }
 })
 </script>
