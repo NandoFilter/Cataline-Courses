43532ec5f1079e2703013e2b3ad221792dd4f77b
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2023-01-25 23:46:04 -0300

    Created NavBar and SearchForm molecules

--- Courses/atomic-design/src/App.vue
+++ Courses/atomic-design/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <SearchForm />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { SearchForm } from '@/components/molecules'
 
-export default defineComponent({})
+export default defineComponent({
+  components: {
+    SearchForm
+  }
+})
 </script>
 
 <style lang="scss" scoped>
