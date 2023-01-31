aebf653910c8a72d8162200fabde4e380a89212e
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2023-01-30 22:12:33 -0300

    Criação dos organismos

--- Courses/atomic-design/src/App.vue
+++ Courses/atomic-design/src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="app">
-    <Service
-      title="Front-End"
-      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea repudiandae, labore eveniet maiores ipsa fugit, ad omnis hic laboriosam magni necessitatibus autem soluta recusandae aliquid similique doloribus! Suscipit, debitis odio!"
-    />
+    <Header />
+    <Presentation />
+    <Services />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Service } from '@/components/molecules'
+import { Header, Presentation, Services } from '@/components/organisms'
 
 export default defineComponent({
   components: {
-    Service
+    Header,
+    Presentation,
+    Services
   }
 })
 </script>
 
 <style lang="scss" scoped>
 .app {
+  margin: 2.5rem;
 }
 </style>
---
+++ Courses/atomic-design/src/components/organisms/Services.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front end"
+      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consequuntur, mollitia unde, vero totam voluptas voluptatem recusandae a culpa veritatis pariatur. Cumque praesentium, odit dicta corrupti dolorum fugiat atque adipisci."
+    />
+    <Service
+      title="Back end"
+      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consequuntur, mollitia unde, vero totam voluptas voluptatem recusandae a culpa veritatis pariatur. Cumque praesentium, odit dicta corrupti dolorum fugiat atque adipisci."
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consequuntur, mollitia unde, vero totam voluptas voluptatem recusandae a culpa veritatis pariatur. Cumque praesentium, odit dicta corrupti dolorum fugiat atque adipisci."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({
+  components: {
+    Service
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
